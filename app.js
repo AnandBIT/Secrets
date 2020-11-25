fc0f826fa189704f1256f324bbd4c670351cf12e
Anand Kumar <kumarabhishek31853@gmail.com>
2020-11-26 03:42:41 +0530

    updated google oauth credentials for deployment

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets"
+        callbackURL: "https://secret-stack.herokuapp.com/auth/google/secrets"
         // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function (accessToken, refreshToken, profile, cb) {
