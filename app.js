9d6a649b10232c03bd81e9e528b5d74ef0b47872
Anand Kumar <kumarabhishek31853@gmail.com>
2020-11-26 04:15:18 +0530

    update

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const sess = {
 };
 
 if (app.get('env') === 'production') {
-    // app.set('trust proxy', 1); // trust first proxy
+    app.set('trust proxy', 1); // trust first proxy
     sess.cookie.secure = true; // serve secure cookies
   }
 
